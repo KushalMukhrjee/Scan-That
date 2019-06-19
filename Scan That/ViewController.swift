aec07917409e2a45b63b1ccd22cc4b8e2a4685b4
Kushal Mukherjee <45260559+KushalMukhrjee@users.noreply.github.com>
2019-06-19 10:26:35 +0530

    Initial Commit

---
+++ Scan That/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scan That
+//
+//  Created by Kushal Mukherjee on 19/06/19.
+//  Copyright © 2019 Kushal Mukherjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
